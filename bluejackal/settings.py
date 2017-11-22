a3f303678986eb113f712ba31325cf7d919cd0cf
marirs <marirs@users.noreply.github.com>
2017-11-22 12:55:28 +0530

    Update settings.py

--- bluejackal/settings.py
+++ bluejackal/settings.py
@@ -122,7 +122,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'blog/static/')
-MEDIA_URL = '/storage/media/'
+MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'storage/media/')
 
 
